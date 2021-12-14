13eac7d19b02fca1b1344b224ff2c7a7ede6c811
wtk <734394415@qq.com>
2021-12-14 08:41:07 +0800

    login_over

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,17 @@
 // 验证码
 let verifyCode = null;
+let layer = layui.layer;
 
 
 
 (function () {
     // 登录与注册切换
-    $('#register').on('click', function () {
+    $('.login #register').on('click', function () {
         console.log(1);
         document.querySelector('.login').style.display = 'none';
         document.querySelector('.register').style.display = 'block';
     })
-    $('#login').on('click', function () {
+    $('.register #login').on('click', function () {
         console.log(1);
         document.querySelector('.login').style.display = 'block';
         document.querySelector('.register').style.display = 'none';
@@ -24,7 +25,6 @@ let verifyCode = null;
 })();
 
 
-console.log(verifyCode);
 
 
 // 表单验证模块
@@ -36,15 +36,64 @@ console.log(verifyCode);
         form.verify({
             pwd: [/^[\S]{6,12}$/, '密码必须为6到12位非空元素'],
             pwd2: function (value) {
-                if ($('#password').val() !== value) {
+                if ($('.register #password').val() !== value) {
                     return '两次密码输入不一致';
                 }
             },
             verify: function (value) {
                 if (!verifyCode.validate(value))
                     return '验证码填写错误';
-
             }
         })
     })
 })();
+
+
+
+// 后台交互模块
+(function () {
+    // 注册模块
+    document.querySelector('.register form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        let user = {};
+        user.username = document.querySelector('.register #username').value;
+        user.password = document.querySelector('.register #password').value;
+        console.log(user);
+        $.ajax({
+            type: "POST",
+            url: "http://127.0.0.1:8080/api/reguser",
+            data: user,
+            success: function (res) {
+                layer.msg(`<span style='color:white'>${res.msg}</span>`, {
+                    anim: 2,
+                    time: 800
+                });
+            },
+        });
+    })
+
+
+    // 登录模块
+    document.querySelector('.login form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        let user = {};
+        user.username = document.querySelector('.login #username').value;
+        user.password = document.querySelector('.login #password').value;
+        console.log(user);
+        $.ajax({
+            type: "POST",
+            url: "http://127.0.0.1:8080/api/login",
+            data: user,
+            success: function (res) {
+                layer.msg(`<span style='color:white'>${res.msg}</span>`, {
+                    anim: 2,
+                    time: 800
+                });
+            },
+        });
+    })
+})();
+
+
+
+
